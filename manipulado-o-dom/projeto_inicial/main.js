99cd7d343925d3dc706f53c513bb2d0a1b87c0ec
Chrystian Oliveira <chrystianmoliveira7@gmail.com>
2022-01-10 21:13:58 -0300

    iniciando projeto com storage e consumindo algumas API basicas em ES

--- manipulado-o-dom/projeto_inicial/main.js
+++ manipulado-o-dom/projeto_inicial/main.js
@@ -8,14 +8,14 @@ const criarTarefa = (evento) => {
     const input = document.querySelector('[data-form-input]');
     const valor = input.value;
 
-    const tarefa = document.createElement('li');
-    tarefa.classList.add('task');
+    const novaTarefa = document.createElement('li');
+    novaTarefa.classList.add('task');
     const conteudo = `<p class="content">${valor}</p>`;
 
-    tarefa.innerHTML = conteudo;
-    tarefa.appendChild(BotaoConclui());
-    tarefa.appendChild(BotaoDeleta());
-    lista.appendChild(tarefa);
+    novaTarefa.innerHTML = conteudo;
+    novaTarefa.appendChild(BotaoConclui());
+    novaTarefa.appendChild(BotaoDeleta());
+    lista.appendChild(novaTarefa);
     input.value = " ";
 }
 
