c759f1959a93a0e24101df73ecb039434266a1c0
Chrystian Oliveira <chrystianmoliveira7@gmail.com>
2022-01-16 23:31:09 -0300

    adicionando json-server e visualizando os usuarios na tela

--- js-na-web/admin/service/cliente-service.js
+++ js-na-web/admin/service/cliente-service.js
@@ -1,13 +1,15 @@
 const criaNovaLinha = (nome, email) => {
     const linhaNovoCliente = document.createElement('tr')
-    const conteudo = `<td class="td" data-td>${nome}</td>
+    const conteudo = `
+    <td class="td" data-td>${nome}</td>
         <td>${email}</td>
         <td>
         <ul class="tabela__botoes-controle">
         <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
         <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
         </ul>
-        </td>`
+        </td>
+        `
     linhaNovoCliente.innerHTML = conteudo
     return linhaNovoCliente
 }
@@ -22,7 +24,7 @@ http.send()
 
 http.onload = () => {
     const data = JSON.parse(http.response)
-    data.forEach((elemento) => {
-        tabela.appendChild = criaNovaLinha(elemento.nome, elemento.email)
+    data.forEach(elemento => {
+        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
     })
 }
